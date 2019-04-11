6c380512fe950996c96ba2c74f43d3299ba0930b
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2019-04-11 17:28:22 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+// Examples use USGS Earthquake API:
+//   https://earthquake.usgs.gov/fdsnws/event/1/#methods
+
+var quakes;
+
+function preload() {
+  let url =
+    'https://earthquake.usgs.gov/earthquakes/feed/v1.0/' +
+    'summary/all_day.geojson';
+  quakes = loadJSON(url);
+}
+
+function setup() {
+  noCanvas();
+
+  var features = quakes.features;
+let total = features.length;
+  for (var i = 0; i < features.length; i++) {
+    
+    let time = features[i].properties.time;
+    let Mag = features[i].properties.mag;
+    let Name = features[i].properties.place;
+    if(Mag > 3){
+    createP('Local: ' + Name);
+    createP('Magnitude ' + Mag);
+    createP('Hora: ' + time);
+      createP('::::::::::::::Sismo ' +i+'/'+total);
+    }
+  }
+  }
