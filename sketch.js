836fb6b1f4956698ce713a5a20e9ee06e3823eaa
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2019-04-12 15:50:22 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 // Examples use USGS Earthquake API:
 //   https://earthquake.usgs.gov/fdsnws/event/1/#methods
-
+var total;
+var tempoLocalms;
 var quakes;
-
+let n = 0; //numero de terremotos com magnitude escolhida
 function preload() {
   let url =
     'https://earthquake.usgs.gov/earthquakes/feed/v1.0/' +
@@ -11,23 +12,61 @@ function preload() {
 }
 
 function setup() {
-  noCanvas();
+  createCanvas(640, 50);
+  createElement('h3','Lista de Terremotos com magnitude acima de 3, nas últimas  24 horas.');
+  createElement('h5','autor: Enivaldo Bonelli, enivaldob@yahoo.com ');
 
   var features = quakes.features;
-let total = features.length;
+  total = features.length;
+
+  //Imprime a hora
+  var currentTime = new Date();
+    dia = currentTime.getDate();
+    hours = currentTime.getHours();
+    minutes = currentTime.getMinutes();
+    mes = currentTime.getMonth()+1;
+    ano = currentTime.getFullYear();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (hours == 0) {
+    hours = 12;
+  }
+  tempoLocalms = currentTime.getTime();
+  createP("Agora é "+dia + "/"+ mes +"/"+ano+" "+ hours + ":" + minutes);
+  //createP("OU");
+  //createP(tempoLocalms + "  Milisegundos");
+
   //Agradecimentos
-  createElement('center','Agradecimentos');
-  createElement('center','Daniel Shiffman, The Processing Foundation');
-  createElement('center','and USGS');
-  for (var i = 0; i < features.length; i++) {  
+  createElement('center', 'Agradecimentos a');
+  createElement('center', 'Daniel Shiffman,');
+  createElement('center', 'The Processing Foundation,');
+  createElement('center', 'and the USGS');
+  for (var i = 0; i < total; i++) {
+
     let time = features[i].properties.time;
     let Mag = features[i].properties.mag;
     let Name = features[i].properties.place;
-    if(Mag > 3){
-    createP('Local: ' + Name);
-    createP('Magnitude ' + Mag);
-    createP('Hora: ' + time);
-      createP('::::::::::::::Sismo ' +i+'/'+total);
+    var sismoTempo = new Date(time);
+    var sismoHora = sismoTempo.getHours();
+    var sismoMin = sismoTempo.getMinutes();
+    var OntemHoje;
+    if(sismoHora > hours){
+     OntemHoje = "Ontem";
+    }else{
+     OntemHoje ="Hoje"; 
+    }
+    if (Mag > 3) {
+      n++;
+      createP('Local: ' + Name);
+      createP('Magnitude ' + Mag);
+      createP('Quando: ' + sismoHora+":"+sismoMin+"   "+OntemHoje);
+      createP('::::::::::::::Sismo ' + n + '/' + i + '/' + total);
     }
   }
-  }
+}
+
+function draw() {
+
+  background(6*n, 0, total-n);
+}
