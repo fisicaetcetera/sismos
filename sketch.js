65a09f024fb285a6eab962f1d7df9e1404a2aa14
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2019-04-15 20:53:59 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,20 +6,34 @@ var total;
 var tempoLocalms;
 var quakes;
 let n = 0; //numero de terremotos com magnitude escolhida
+let assinatura;
+var lat = [];
+var long = [];
+
 function preload() {
+
   terra = loadImage('earthmap1k.jpg');
-  let url =
-    'https://earthquake.usgs.gov/earthquakes/feed/v1.0/' +
-    'summary/all_day.geojson';
+  //let url = 'all_day.geo.json';
+   let url='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
   quakes = loadJSON(url);
 }
 
 function setup() {
-  createCanvas(640, 480, WEBGL);
+  //noLoop();
+  //saveJSON(quakes, 'all_day.geo.json', false);
+  createCanvas(640, 300, WEBGL);
+  assinatura = createGraphics(10, 10);
+  assinatura.background(255,55);
+  assinatura.fill(0);
+  assinatura.textAlign(CENTER);
+  assinatura.textSize(150);
+  assinatura.text('4.5', 0, 0);
+  createP('^^^Vou colocar os sismos nas figuras. Aguardem')
   createElement('h3', 'Lista de Terremotos com magnitude acima de 3, nas últimas  24 horas.');
   createElement('h5', 'autor: Enivaldo Bonelli, enivaldob@yahoo.com ');
 
   var features = quakes.features;
+  var geometry = quakes.geometry;
   total = features.length;
 
   //Imprime a hora
@@ -50,6 +64,11 @@ function setup() {
     let time = features[i].properties.time;
     let Mag = features[i].properties.mag;
     let Name = features[i].properties.place;
+    //====================
+    lat[i] = features[i].geometry.coordinates[1];
+    long[i] = features[i].geometry.coordinates[0];
+    //print(long, lat);
+    //====================
     var sismoTempo = new Date(time);
     var sismoHora = sismoTempo.getHours();
     var sismoMin = sismoTempo.getMinutes();
@@ -70,16 +89,24 @@ function setup() {
 }
 
 function draw() {
-
-  background(0);
-  angle += 0.001;
-  //pointLight(255,0,0,-200,0,0);
-  //directionalLight(255, 255, 0, 1, 0, 0);
-  directionalLight(150, 150, 150, 0, 0, -1);
-  //pointLight(255, 0,0,0,-200,0);
-  rotateY(angle);
-  //noStroke();
+  push();
   texture(terra);
-  sphere(200);
-  
+  translate(0,0);
+  plane(640, 300);
+  pop();
+for(var j = 0; j<total; j++){
+  //print(lat[0], long[0]);
+  let yylat = map(lat[j], 90, -90, -height/2,height/2, true);
+  let xlong = map(long[j],-180,180,-width/2,width/2,true);
+  //print(yylat, xlong);
+  //***
+  push();
+  //translate(0,-85)
+  translate(xlong,yylat);
+  texture(assinatura);
+  rotateX(1.5);
+  fill(255,255,0,88)
+  cylinder(6, 5);
+  pop();
+}
 }
