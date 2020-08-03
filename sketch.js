ccf5974289589712d389c5eb3ad73473f4573ec5
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-08-03 18:00:53 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ let degtorad = 57.296;
 function preload() {
 
   terra = loadImage('earthmap1k.jpg');
-  moon = loadImage('moontexture.jpg');
+  moon = loadImage('moonmap1k.jpg');
   //let url = 'all_day.geo.json';
   let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
   quakes = loadJSON(url);
