a4cc631da3009e8c740c2677433f2ac82acc8a61
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2023-09-01 21:12:04 -0300

    Update sketch.js
    Increased canvas dimensions by factor 2.

--- sketch.js
+++ sketch.js
@@ -119,7 +119,7 @@ function setup() {
   //noLoop();
   //saveJSON(quakes, 'all_day.geo.json', false);
   //
-  createCanvas(640, 300, WEBGL);
+  createCanvas(1280, 600, WEBGL);
   //
   //assinatura = createGraphics(10, 10);
   //assinatura.background(255, 55);
