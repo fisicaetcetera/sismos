c9d953594211e7868f14a655144886ef6fc1817e
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2023-09-13 15:54:38 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -103,7 +103,7 @@ function preload() {
   terra = loadImage('earthmap1k.jpg');
   moon = loadImage('moonmap1k.jpg');
   sol = loadImage('sun.jpg');
-  myFont = loadFont('cattalina.otf');
+  myFont = loadFont('Catallina.otf');
   //let url = 'all_day.geo.json';
   let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
   quakes = loadJSON(url);
